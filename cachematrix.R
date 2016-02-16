6be863e325141625cb20e013cbcc74cba9d382bb
Tim Coker <cokert@gmail.com>
2016-02-16 06:58:11 -0500

    Assignment implementation
    Implementation of the assignment from week 3.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These function implement a wrapper on a matrix that will cache the calculation of its inverse.
+##  Example of use: 
+##      mc <- makeCache(m)  # where m is an invertible matrix
+##      cacheSolve(mc)      # note that the return from the call to makeCache is passed to 
+##                          # cacheSolve, NOT the matrix m
 
-## Write a short comment describing this function
+## returns a wrapper on a matrix that provides facilities for storing its inverse
+##  Functions available: 
+##    set(m):       change the matrix that's saved (invalidates the cache, if there is one)
+##    get():        return the stored matrix
+##    setsolve(m):  stores the the inverted matrix
+##    getsolve():   returns the inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## This function is used to simplify the use of makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
